2307dcb26241ab47a5286f81e8efe257eed97755
Luis Huacho <l.huacho@gmail.com>
2024-06-09 20:53:17 -0500

    Grupo Musical y Author como tipos de contenidos fueron añadidos Music Group and Author contents type was added

--- cancionero/rock/admin.py
+++ cancionero/rock/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import GrupoMusical
+from .models import Author
+
+
+# admin.site.register(GrupoMusical)
+
+
+class GrupoMusicalAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'modified')
+    list_display = ('title', 'author', 'created', 'modified')
+    date_hierarchy = 'created'
+
+
+admin.site.register(GrupoMusical, GrupoMusicalAdmin)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name' , 'last_name')
+
+
+admin.site.register(Author, AuthorAdmin)
--- cancionero/rock/models.py
+++ cancionero/rock/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
+
+class GrupoMusical(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    my_field_name = models.CharField(max_length=20, help_text="Enter field documentation", blank=True)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Grupo musical"
+        verbose_name_plural = "Grupos musicales"
+
+
+class Author(models.Model):
+    """
+    Modelo que representa un autor
+    """
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def get_absolute_url(self):
+        """
+        Retorna la url para acceder a una instancia particular de un autor.
+        """
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """
+        String para representar el Objeto Modelo
+        """
+        return '%s, %s' % (self.last_name, self.first_name)
